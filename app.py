896e3474fa193d49d483831ea066f95e9d6a9875
Nguyễn Phước Bình <106498497+Phuocbinh2003@users.noreply.github.com>
2024-09-22 20:07:12 +0700

    Update app.py

--- app.py
+++ app.py
@@ -3,35 +3,34 @@ import cv2 as cv
 import numpy as np
 from character_segmentation import process_image  # Import hàm từ tệp phân đoạn ký tự
 
-# Tiêu đề ứng dụng
+
 st.title("Ứng dụng Xử lý Hình ảnh")
 
-# Tải ảnh từ GitHub
-image_url = "https://raw.githubusercontent.com/username/repo_name/main/images/ndata96.jpg"  # Cập nhật đường dẫn đúng
 
-# Đọc ảnh từ URL
+image_url = "ndata96.jpg"  
+
+
 image_response = requests.get(image_url)
 uploaded_file = image_response.content
 
-# Xử lý ảnh
 original, binary, dilated, dist_transform, img_markers = process_image(uploaded_file)
 
-# Hiển thị ảnh gốc
+
 st.subheader("Ảnh gốc")
 st.image(original, channels="BGR")
 
-# Hiển thị ảnh nhị phân
+
 st.subheader("Ảnh nhị phân")
 st.image(binary, use_column_width=True, clamp=True, channels="GRAY")
 
-# Hiển thị ảnh sau khi mở rộng
+
 st.subheader("Ảnh sau khi mở rộng")
 st.image(dilated, use_column_width=True, clamp=True, channels="GRAY")
 
-# Hiển thị Distance Transform
+
 st.subheader("Distance Transform")
 st.image(dist_transform, use_column_width=True, clamp=True, channels="GRAY")
 
-# Hiển thị ảnh Watershed
+
 st.subheader("Ảnh Watershed Segmentation")
 st.image(img_markers, channels="BGR")
